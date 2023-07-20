865884a5f73195e62aa666eef308b3c049bb2b6e
Apurva Dhundur <dhundura@gmail.com>
2023-07-20 12:01:50 +0100

    Setup routing for profile and projects

--- static/src/app/app.component.ts
+++ static/src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from 'primeng/api';
+import { MenuItem, PrimeIcons } from 'primeng/api';
+import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  title = 'static';
+export class AppComponent implements OnInit {
+
+  items: MenuItem[] | undefined;
+  faMagnifyingGlass = faMagnifyingGlass;
+
+  constructor(private primengConfig: PrimeNGConfig) { }
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+
+    this.items = [
+      {
+        label: 'Profile',
+        icon: PrimeIcons.USER,
+        routerLink: '/profile'
+      },
+      {
+        label: 'Projects',
+        icon: PrimeIcons.BOOK,
+        routerLink: '/projects'
+      }
+    ];
+  }
 }
--- static/src/app/app.module.ts
+++ static/src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenubarModule } from 'primeng/menubar';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ProfileComponent } from './profile/profile/profile.component';
+import { ProjectsComponent } from './projects/projects/projects.component';
+
+const appRoutes: Routes = [
+  { path: '', component: ProfileComponent },
+  { path: 'profile', component: ProfileComponent },
+  { path: 'projects', component: ProjectsComponent },
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent,
+    ProjectsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MenubarModule,
+    FontAwesomeModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
